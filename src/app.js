c196c100bf59569a05f1c82f712ed88b61c470d4
Scott Lingner <slingner@gmail.com>
2020-01-27 09:02:58 -0800

    setup bookmarks endpoints

--- src/app.js
+++ src/app.js
@@ -1,34 +1,137 @@
 //Configure create variables written in .env file to the environment
 require('dotenv').config();
-//imports Express 
+
 const express = require('express');
-//Enable middleware functions
 const morgan = require('morgan');
-//Enable Cross Origin Resource Sharing, simplifies the config of CORS in Express
 const cors = require('cors');
-//does the security hiding for us
 const helmet = require('helmet');
-//imports environment from config settings
 const { NODE_ENV } = require('./config');
+const winston = require('winston');
+const uuid = require('uuid/v4');
+const { bookmarks } = require('./store');
+
 
 const app = express();
 
-// Depends on the condition of the environment
-// Morgan - tiny format for production environment
-// Morgan - common format for development environment
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-  // Mounting our middleware, always put helmet before cors!
+// Mounting our middleware, always put helmet before cors!
 app.use(morgan(morganOption)); 
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+
 
 
 app.get('/', (req, res) => {
   res.json( { message: 'Hello World' } );
 });
+
+app.get('/bookmarks', (req, res) => {
+  res
+    .json(bookmarks)
+});
+
+app.get('/bookmarks/:id', (req, res) => {
+  const { id } = req.params; //this is the users the id 
+  const bookmark = bookmarks.find(b => b.id == id); 
+
+  //make sure we found a bookmark
+  if(!bookmark) {
+    logger.error('Bookmark with id ${id} not found.');
+    return res 
+      .status(404)
+      .send('Card Not Found');
+  }
+  res
+    .json(bookmarks)
+});
+
+app.post('/bookmarks', (req, res) => {
+  const { title, url, description = '', rating } = req.body; 
+
+  if( !title || !url || !rating ) {
+    logger.error('Title, Url and Rating are required');
+    return res
+    .status(400)
+    .send('Invalid data');
+  }
+  const id = uuid();
+
+  const bookmark = {
+    id,
+    title,
+    url,
+    description,
+    rating
+  };
+  
+  bookmarks.push(bookmark)
+
+  logger.info( `Bookmark with id ${id} created`);
+
+  res
+    .status(201)
+    .location(`http://localhost:8000/bookmarks/${id}`)
+    .json(bookmark);
+})
+
+app.delete('/bookmarks/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = bookmarks.findIndex(bookmark => bookmark.id == id);
+
+  if(index === -1) { 
+    logger.error(`List with id ${ id } not found.`);
+    return res
+      .status(404)
+      .end();
+  }
+
+  bookmarks.splice(index, 1);
+
+  logger.info(`List with id ${id} deleted.`);
+  res
+    .status(204)
+    .end();
+})
+
+
+
+
+
+
+
+
 
 app.use((error, req, res, next) => {
   let response;
