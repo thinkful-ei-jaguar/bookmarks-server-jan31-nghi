f704ccf527412e0e16c2565a6942a5efc35b7cec
Nghi Tran <nghi.t.tran8@gmail.com>
2020-01-27 09:30:16 -0800

    more organizing

--- src/app.js
+++ src/app.js
@@ -9,19 +9,17 @@ const { NODE_ENV } = require('./config');
 const bookmarkRouter = require('./bookmarkRoute');
 const winston = require('winston');
 const logger = require('./logger');
+const validateToken = require('./validateToken');
+const errorHandling = require('./errorHandling');
 
 
 const app = express();
-
-
 
 if (NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.simple()
   }));
 }
-
-
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
@@ -31,34 +29,12 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use((error, req, res, next) => {
-  let response;
-  if(NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-
-  res.status(500).json(response);
-});
-
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  // move to the next middleware
-  next()
-})
+app.use(errorHandling);
+app.use(validateToken);
 
 
 // Routes
 app.use(bookmarkRouter);
-
 app.get('/', (req, res) => {
   res.json( { message: 'Hello World' } );
 });
---
+++ src/errorHandling.js
@@ -0,0 +1,11 @@
+module.exports = function(error, req, res, next) {
+    let response;
+    if(NODE_ENV === 'production') {
+      response = { error: { message: 'server error' } };
+    } else {
+      console.error(error);
+      response = { message: error.message, error };
+    }
+  
+    res.status(500).json(response);
+  };
